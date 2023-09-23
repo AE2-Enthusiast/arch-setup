cf4f5925aea7e906fecb5f9f5fc91e1f9f323ded
Stone <samstone9001@gmail.com>
2023-09-23 18:18:09 -0400

    attempts to add marquee to polybar

--- theme/theme.sh
+++ theme/theme.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+cd /home/stone/git/arch-setup/theme
+
 cp $1.m4 theme.m4
 
 m4 i3/template.m4 -I i3/ -I lib/ > ../config/.config/i3/config
@@ -7,5 +9,6 @@ m4 alacritty/template.m4 -I alacritty/ -I lib/ > ../config/.config/alacritty/\
 alacritty.toml
 m4 polybar/template.m4 -I polybar/ -I lib/ > ../config/.config/polybar/config.ini
 
-i3-msg restart
+i3-msg reload
+polybar-msg cmd restart
 feh --bg-center background/$1.*
